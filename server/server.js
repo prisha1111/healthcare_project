4ebba0466f08e5d1e747148d7ba74891c8731230
My Name <prisha682.be22@chitkara.edu.in>
2024-10-23 15:27:35 +0530

    hbs,partials,nodemon

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const connectDb=require("./config/dbConnection");
 const errorHandler=require("./middleware/errorHandler"); //when server is giving error, error handle krne ke liye errorhandling, errorhandling is kind of middleware //middleware->acts as a gaurd
 const cors = require("cors"); //helps in identity verify, so that safety breach does not happen while getting apis
 
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 //env file config
 const dotenv = require("dotenv");
 dotenv.config();
@@ -24,9 +27,23 @@ app.get('/',(req,res)=>{
     res.send("working");
 });
 
+app.get("/home",(req,res)=>{
+    //let user=User.findOne({id:})
+    res.render("home",{})
+})
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users:[{id:1,username:"Nitesh",
+            age:23},{id:1,username:"Akash",
+                age:56
+            }]
+    });
+})
 
 //app config start
 app.listen(port, ()=>{
     console.log(`Server running on port http://localhost:${port}`);
 });
 
+app.set('view engine', 'hbs');
